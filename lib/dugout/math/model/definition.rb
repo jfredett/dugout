740aa9578247373be6c9761fd175e5e8f8c0e96b
Joe Fredette <jfredett@gmail.com>
2013-09-09 23:09:00 -0400

    add definition of EqDL

--- lib/dugout/math/model/definition.rb
+++ lib/dugout/math/model/definition.rb
@@ -1,4 +1,79 @@
 module Dugout
   module Math
+    module Model
+      Defn = model do
+        primitive_op :Multiplication do
+          attribute :left
+          attribute :right
+
+          operator '*'
+        end
+
+        primitive_op :Addition do
+          attribute :left
+          attribute :right
+
+          operator '+'
+        end
+
+        primitive_op :Variable do
+          attribute :name
+
+          operator 'var'
+
+          display_function { name }
+        end
+
+        primitive_op :Literal do
+          attribute :value
+
+          operator 'lit'
+
+          display_function { value }
+        end
+
+        primitive_op :Exponential do
+          attribute :value
+
+          operator 'exp'
+
+          display_function { "exp(# @returns {value})" }
+        end
+
+        primitive_op :Log do
+
+        end
+
+        derived_op :Subtraction do
+          attribute :left
+          attribute :right
+
+          operator '-'
+
+          implementation do
+            left + (lit(-1) * right)
+          end
+        end
+
+        derived_op :Power do
+          implementation do
+            exp(log(left) * right)
+          end
+
+          operator '**'
+        end
+
+        derived_op :Sqrt do
+          attribute :radicand
+
+          operator 'root'
+
+          implementation do
+            radicand ** (lit(1) / lit(2))
+          end
+
+          display_function { "sqrt(# @returns {value})" }
+        end
+      end
   end
 end
---
+++ spec/unit/math/model/*_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Dugout::Math::Model do
+  describe Dugout::Math::Model::Defn do
+    subject { Dugout::Math::Model::Defn }
+
+    it { should be_a Dugout::Math::Parser::Model }
+  end
+end
