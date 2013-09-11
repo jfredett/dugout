0d008484b887e0ef8a567a944ec374d34d44332b
Joe Fredette <jfredett@gmail.com>
2013-09-11 08:57:49 -0400

    Document and Refactor OpCompiler

--- lib/dugout/math/model/op_compiler.rb
+++ lib/dugout/math/model/op_compiler.rb
@@ -5,6 +5,13 @@ module Dugout
       # A Unit-of-work style class for turning a Parser::PrimitiveOp chunk of
       # the Model definition AST into a real expression-AST class
       class OpCompiler
+        attr_reader :location, :ast
+
+        extend Forwardable
+
+        delegate [:name, :attributes] => :ast
+        def_delegator :attributes, :length, :arity
+
         ##
         # Create a new Unit-of-work style compiler for the given chunk of the
         # model-definition AST.
@@ -18,39 +25,53 @@ module Dugout
           @location = location
         end
 
+        ##
+        # Custom iterator for dealing with attributes
+        def each_attribute_by_name
+          Array(attributes).each do |attr|
+            yield attr.name, attr if block_given?
+          end
+        end
+
+        ##
+        # Lazily initializes the operator class in the given location
+        #
+        # @return [Class] the compiled op's class
+        def op_class
+          @op_class ||= location.const_set(ast.name, Class.new)
+        end
+
+        ##
         # Cause the Operator class to be defined in Dugout::Math::Model
         def run!
-          location.const_set(ast.name, Class.new)
+          define_initializer!
+          define_attribute_getters!
+        end
 
+        private
+
+        ##
+        # Define the initializer on the compiled op's class
+        def define_initializer!
           compiler = self
-          location.const_get(name).class_eval do
+          op_class.class_eval do
             define_method(:initialize) do |*args|
               raise ArgumentError unless args.length == compiler.arity
               args.zip(compiler.attributes).each do |arg, attr|
                 instance_variable_set("@#{attr.name}", arg)
               end
             end
-
-            compiler.each_attribute_by_name do |name|
-              define_method(name) do
-                instance_variable_get("@#{name}")
-              end
-            end
           end
         end
 
-        attr_reader :location, :ast
-
-        extend Forwardable
-
-        delegate [:name, :attributes] => :ast
-        def_delegator :attributes, :length, :arity
-
         ##
-        # Custom iterator for dealing with attributes
-        def each_attribute_by_name
-          Array(attributes).each do |attr|
-            yield attr.name, attr if block_given?
+        # Define each attribute's getter on the compiled op's class
+        def define_attribute_getters!
+          compiler = self
+          op_class.class_eval do
+            compiler.each_attribute_by_name do |name|
+              define_method(name) { instance_variable_get("@#{name}") }
+            end
           end
         end
       end
