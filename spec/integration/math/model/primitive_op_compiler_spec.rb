0c3d5ba6d139726e21c5ad8787e3304a53a95779
Joe Fredette <jfredett@gmail.com>
2013-09-11 09:01:57 -0400

    move specs to appropriate location

--- spec/integration/math/model/primitive_op_compiler_spec.rb
+++ spec/integration/math/model/primitive_op_compiler_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+# integrates across the Compiled model and the parser
 describe Dugout::Math::Model::PrimitiveOpCompiler do
   it_behaves_like 'an op compiler for a', Dugout::Math::Parser::PrimitiveOp do
     with_compiler_class Dugout::Math::Model::PrimitiveOpCompiler 
