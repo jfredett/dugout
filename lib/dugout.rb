97f76f272d52522d119a42f440821d6d4129ef13
Joe Fredette <jfredett@gmail.com>
2013-09-13 18:12:10 -0400

    Remove explicit definition of namespace from Reifier

--- lib/dugout.rb
+++ lib/dugout.rb
@@ -3,4 +3,6 @@ require "dugout/version"
 require 'katuv'
 require 'dugout/math'
 
+Dugout::Math::Model::Reifier.clean!
+
 module Dugout; end
--- lib/dugout/math/model/reifier.rb
+++ lib/dugout/math/model/reifier.rb
@@ -1,19 +1,16 @@
 module Dugout
   module Math
     module Model
-      module Expression
-        module Evaluator; end
-        module Language ; end
-      end
-
       module Reifier
         def self.compile!
+          clean!
           Dugout::Math::Model.ops.each do |op|
             OpCompiler.new(op, Expression::Language, Expression::Evaluator).run!
           end
         end
 
         def self.clean!
+          Dugout::Math::Model.const_set(:Expression, Module.new)
           Dugout::Math::Model::Expression.const_set(:Evaluator, Module.new)
           Dugout::Math::Model::Expression.const_set(:Language, Module.new)
         end
